bddce1573827244c9e9053734e94d38e72ca8987
oveshak <oveshaktoton9999@gmail.com>
2026-05-08 18:08:52 +0600

    add file

--- src/lib/api/baseurl.tsx
+++ src/lib/api/baseurl.tsx
@@ -1,2 +1,3 @@
- export const url='https://skillscraftacademy.com'
-//export const url='http://127.0.0.1:8000'
+//  export const url='https://skillscraftacademy.com'
+// export const url='http://127.0.0.1:8000'
+export const url='https://skillcarftbackend.vercel.app'
